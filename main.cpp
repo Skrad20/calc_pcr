68eef40c662ade44c7833b8d2f7be74cffae7870
unknown <skrad200052@yandex.ru>
2022-06-02 14:17:15 +0300

    0.0.1 - Add calc regent exrection

--- main.cpp
+++ main.cpp
@@ -1,44 +1,133 @@
-
+#include <memory>
 #include <iostream>
 #include <string>
 using namespace std;
 
-
-struct data_calculation{
+struct calculator_pcr{
   /*
-  Данные для расчёта.
+    Калькулятор количества реагентов для ЦПР.
   */
-  data_calculation(double  * const amount_dna,
-        double * const number_samples,
-        double * const volume_mixture,
-        int * const var_analis) 
-: amount_dna(amount_dna), number_samples(number_samples), 
-volume_mixture(volume_mixture), var_analis(var_analis){};
-  double *amount_dna = 0;
-  double *number_samples = 0;
-  double *volume_mixture = 0;
-  int *var_analis = 0;
+  calculator_pcr(
+      int number_samples,
+      int var_pcr,
+      double volume_mixture,
+      double amount_dna
+    )
+    : number_samples(number_samples), var_pcr(var_pcr),
+    volume_mixture(volume_mixture), amount_dna(amount_dna) {};
+
+  void set_new_value(int new_number_samples){
+    this->bufer = bufer * new_number_samples;
+    this->TAG = TAG * new_number_samples;
+    this->dNTP = dNTP * new_number_samples;
+    this->primer = primer * new_number_samples;
+    this->h2o = (h2o * new_number_samples) - bufer - TAG - dNTP - (primer * var_pcr);
+  }
+    void set_new_value(){
+    this->bufer = bufer * number_samples;
+    this->TAG = TAG * number_samples;
+    this->dNTP = dNTP * number_samples;
+    this->primer = primer * number_samples;
+    this->h2o = (h2o * number_samples) - bufer - TAG - dNTP - (primer * var_pcr);
+  }
+  void print_res(){
+    cout << "--------------------------------------------------------" <<endl;
+    cout << "Reagent:        Volume"<< endl;
+    cout << "10x bufer:      " << bufer << " uL."<< endl;
+    cout << "dNTP:           " << dNTP << " uL."<< endl;
+
+    if (var_pcr == 1){
+      cout << "Primer:         " << primer << " uL."<< endl;
+    } else if (var_pcr == 2){
+      cout << "Primer F:       " << primer << " uL."<< endl;
+      cout << "Primer R:       " << primer << " uL."<< endl;
+    }
+    
+    cout << "Taq-polymerase: " << TAG << " uL." << endl;
+    cout << "Water:          " << h2o << " uL." <<endl;
+    cout << "DNA:            " << amount_dna << " uL." << endl;
+    cout << "--------------------------------------------------------" <<endl;
+}
+
+  int number_samples = 0;
+  int var_pcr = 0;
+  private:
+    double volume_mixture = 15;
+    double amount_dna = 1;
+    double bufer = volume_mixture * 0.1;
+    double TAG = volume_mixture * 0.024;
+    double h2o = volume_mixture - amount_dna;
+    double dNTP = volume_mixture * 0.08;
+    double primer = volume_mixture * 0.02;
+    
 };
 
-struct data_outer{
+struct calculator_extraction{
   /*
-  Данные для вывода.
+  Калькулятор количества реагентов для выделения.
   */
-  data_outer(
-        ){};
-    data_outer(
-        double bufer,
-        double TAG,
-        double h2o,
-        double dNTP,
-        double primer
-        ) 
-: bufer(bufer), TAG(TAG), h2o(h2o), dNTP(dNTP), primer(primer){};
-  double bufer = 0;
-  double TAG = 0;
-  double h2o = 0;
-  double dNTP = 0;
-  double primer = 0;
+  calculator_extraction(
+      int number_samples,
+      int var_extraction
+    )
+    : number_samples(number_samples), var_extraction(var_extraction){};
+
+  void set_new_value(){
+    this->proteinase = proteinase * (number_samples);
+    this->lys_sol_1 = lys_sol_1 * (number_samples);
+    this->lys_sol_2 = lys_sol_2 * (number_samples);
+    this->precipitating_sol_1 = precipitating_sol_1 * (number_samples);
+    this->precipitating_sol_2 = precipitating_sol_2 * (number_samples);
+    this->washing_sol = washing_sol * (number_samples);
+    this->eluiting_sol = eluiting_sol * (number_samples);
+    this->b_mercaptoethanol = b_mercaptoethanol * (number_samples);
+    this->gkycogen = gkycogen * (number_samples);
+  }
+    void set_new_value(int new_number_samples){
+    this->proteinase = proteinase * new_number_samples;
+    this->lys_sol_1 = lys_sol_1 * new_number_samples;
+    this->lys_sol_2 = lys_sol_2 * new_number_samples;
+    this->precipitating_sol_1 = precipitating_sol_1 * new_number_samples;
+    this->precipitating_sol_2 = precipitating_sol_2 * new_number_samples;
+    this->washing_sol = washing_sol * new_number_samples;
+    this->eluiting_sol = eluiting_sol * new_number_samples;
+    this->b_mercaptoethanol = b_mercaptoethanol * new_number_samples;
+    this->gkycogen = gkycogen * new_number_samples;
+  }
+  void print_res(){
+    cout << "--------------------------------------------------------" <<endl;
+    cout << "Reagent:                  Volume"<< endl;
+    cout << "Proteinase K:             " << proteinase << " uL."<< endl;
+    if (var_extraction == 1){
+      cout << "Lysing solution 1:        " << lys_sol_1 << " uL."<< endl;
+    }
+    cout << "Lysing solution 2:        " << lys_sol_2 << " uL."<< endl;
+    if (var_extraction == 2){
+      cout << "2 mercaptoethanol:        " << b_mercaptoethanol << " uL."<< endl;
+      cout << "Glycogen:                 " << gkycogen << " uL." << endl;
+    }
+
+    cout << "Precipitating solution 1: " << precipitating_sol_1 << " uL."<< endl;
+    cout << "Precipitating solution 2: " << precipitating_sol_2 << " uL."<< endl;
+    cout << "Washing solution:         " << washing_sol << " uL." << endl;
+    cout << "Eluiting sollution:       " << eluiting_sol << " uL." <<endl;
+    cout << "--------------------------------------------------------" <<endl;
+    
+  }
+  
+  int number_samples = 0;
+  int var_extraction = 0;
+  private:
+    double proteinase = 8;
+    double lys_sol_1 = 900;
+    double lys_sol_2 = 300;
+    double precipitating_sol_1 = 100;
+    double precipitating_sol_2 = 300;
+    double washing_sol = 400;
+    double eluiting_sol = 50;
+    double b_mercaptoethanol = 1;
+    double gkycogen = 2;
+
 };
 
 void requesting_amount_dna(double *amount_dna){
@@ -49,7 +138,7 @@ void requesting_amount_dna(double *amount_dna){
   cin >> *amount_dna;
 }
 
-void requesting_number_samples(double *number_samples){
+void requesting_number_samples(int *number_samples){
   /*
   Запрос количества проб.
   */
@@ -70,50 +159,25 @@ void requesting_variant_analis(int *var_analis){
   Запрос пунтка меню.
   */
   cout << "Select an item:" << endl
+      << "PCR" << endl
       << "1 - ISSR" << endl
       << "2 - Standard genotype study" << endl
       << "3 - Change the standard volume of the mixture" << endl
+      << "Extraction DNA" << endl
+      << "4 - Reagents for extraction" << endl
       << "0 - Exit" << endl;
   cin >> *var_analis;
 }
 
-void output_data(data_calculation const * const data, data_outer const * const data_out){
+void requesting_variant_analis_extraction(int *var_analis_for_extraction){
   /*
-  Отрисовывает результаты расчёта.
+  Запрос подпунтка меню для выделения ДНК.
   */
-    cout << "10x bufer:      " << (data_out->bufer * (*data->number_samples + 1)) << " uL."<< endl;
-    cout << "dNTP:           " << (data_out->dNTP * (*data->number_samples + 1)) << " uL."<< endl;
-
-    if (*data->var_analis == 1){
-    cout << "Primer:         " << (data_out->primer * (*data->number_samples + 1)) << " uL."<< endl;
-    } else if (*data->var_analis == 2){
-    cout << "Primer F:       " << (data_out->primer * (*data->number_samples + 1)) << " uL."<< endl;
-    cout << "Primer R:       " << (data_out->primer * (*data->number_samples + 1)) << " uL."<< endl;
-    }
-    cout << "Taq-polymerase: " << (data_out->TAG * (*data->number_samples + 1)) << " uL." << endl;
-    cout << "Water:          " << (data_out->h2o * (*data->number_samples + 1)) << " uL." <<endl;
-    cout << "DNA:            " << (*data->amount_dna) << " uL." << endl;
-}
-
-void calculation(data_calculation const * const data, data_outer * data_out){
-  /*
-  Заполняет данные результата расчёта.
-  */
-  double h2o = *data->volume_mixture;
-  double bufer = h2o * 0.1;
-  double dNTP = h2o * 0.08;
-  double primer = h2o * 0.02;
-  double TAG = h2o * 0.024;
-  if (*data->var_analis == 1){
-    h2o = h2o - bufer - dNTP - TAG - primer - *data->amount_dna;
-  } else if (*data->var_analis == 2){
-    h2o = h2o - bufer - dNTP - TAG - primer - primer - *data->amount_dna;
-  }
-  data_out->bufer = bufer;
-  data_out->TAG = TAG;
-  data_out->h2o = h2o;
-  data_out->dNTP = dNTP;
-  data_out->primer = primer;
+  cout << "Select an item:" << endl
+    << "1 - DNA-Extran-1" << endl
+    << "2 - DNA-Extran-2" << endl
+    << "0 - Exit" << endl;  
+  cin >> *var_analis_for_extraction;
 }
 
 void programma_menu(){
@@ -121,45 +185,34 @@ void programma_menu(){
   Основной процесс.
   */
   double amount_dna = 0;
-  double number_samples = 0;
+  int number_samples = 0;
   double volume_mixture = 15;
   int var_analis = -1;
+
   while (var_analis != 0){
     requesting_variant_analis(&var_analis);
+
     if (var_analis == 0){
+      /* Выход */
       cout << "exit!" << endl;
       break;
-    } else if(var_analis == 1) {
+
+    } else if(var_analis == 1 || var_analis == 2) {
+      /* Отделение ПЦР расчёты */
       cout << "Total volume of the mixture: " << volume_mixture <<" uL." << endl;
       requesting_amount_dna(&amount_dna);
       requesting_number_samples(&number_samples);
-      data_calculation *data = new data_calculation(
-        &amount_dna,
-        &number_samples,
-        &volume_mixture,
-        &var_analis
-      );
-      data_outer *data_out = new data_outer();
-      calculation(data, data_out);
-      output_data(data, data_out);
-      delete data;
-      delete data_out;
-    }else if(var_analis == 2) {
-      cout << "Total volume of the mixture: " << volume_mixture <<" uL." << endl;
-      requesting_amount_dna(&amount_dna);
-      requesting_number_samples(&number_samples);
-      data_calculation *data = new data_calculation(
-        &amount_dna,
-        &number_samples,
-        &volume_mixture,
-        &var_analis
-      );
-      data_outer *data_out = new data_outer();
-      calculation(data, data_out);
-      output_data(data, data_out);
-      delete data;
-      delete data_out;
+      
+      unique_ptr<calculator_pcr> data(
+          new calculator_pcr(
+            number_samples, var_analis, volume_mixture, amount_dna
+          )
+        );
+        data->set_new_value();
+        data->print_res();
+
     } else if (var_analis == 3){
+      /* Отделение изменения объёма смеси */
       requesting_volume_mixture(&volume_mixture);
       if (volume_mixture > 0){
         cout << "New values are set." << endl;
@@ -168,14 +221,37 @@ void programma_menu(){
         cout << "Incorrect values have been entered." << endl;
         requesting_volume_mixture(&volume_mixture);
       }
-    }else {
+
+    } else if(var_analis = 4){
+      /* Отделение расчёт выделения */
+      requesting_number_samples(&number_samples);
+      int var_analis_extra = -1;
+      requesting_variant_analis_extraction(&var_analis_extra);
+      
+      if (var_analis_extra == 0){
+        continue;
+      } else if (var_analis_extra == 1 || var_analis_extra == 2){
+
+        unique_ptr<calculator_extraction> data(
+          new calculator_extraction(
+            number_samples, var_analis_extra
+          )
+        );
+        data->set_new_value();
+        data->print_res();
+      } else {
+        cout << "Incorrect data entered!" << endl;
+        cout << "Select an item from the list." << endl;
+        continue;
+      }
+      
+    } else {
       cout << "Incorrect data entered!" << endl;
       cout << "Select an item from the list." << endl;
       continue;
     }
   }
 }
-
 
 int main() {
     setlocale(LC_ALL, "Russian");
